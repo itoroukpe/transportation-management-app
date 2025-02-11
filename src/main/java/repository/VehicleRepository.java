cfd2605a66758dfe00557012df21f224d4ddabb6
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-02-10 22:22:05 -0800

    Update VehicleRepository.java

--- src/main/java/repository/VehicleRepository.java
+++ src/main/java/repository/VehicleRepository.java
@@ -1,1 +1,24 @@
+package com.example.transportation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "vehicles")
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String licensePlate;
+    private String model;
+    private String manufacturer;
+    private int year;
+}
 
