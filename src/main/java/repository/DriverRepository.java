6d8d896a1658fab879d43a7b69c4511db653a88a
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-02-10 22:22:28 -0800

    Update DriverRepository.java

--- src/main/java/repository/DriverRepository.java
+++ src/main/java/repository/DriverRepository.java
@@ -1,1 +1,10 @@
+package com.example.transportation.repository;
+
+import com.example.transportation.model.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+}
 
