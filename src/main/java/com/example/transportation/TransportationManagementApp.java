47ff4cabece064874efad6048c43c99d96cd0c4d
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-02-10 22:18:47 -0800

    Update TransportationManagementApp.java

--- src/main/java/com/example/transportation/TransportationManagementApp.java
+++ src/main/java/com/example/transportation/TransportationManagementApp.java
@@ -1,1 +1,12 @@
+package com.example.transportation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportationManagementApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TransportationManagementApp.class, args);
+    }
+}
 
