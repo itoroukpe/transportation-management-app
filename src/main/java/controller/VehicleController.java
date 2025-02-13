45e6c23433e68c153d6124f80305295f40294f34
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-02-12 21:52:27 -0800

    Update VehicleController.java

--- src/main/java/controller/VehicleController.java
+++ src/main/java/controller/VehicleController.java
@@ -1,27 +1,17 @@
-package com.example.transportation.controller;
-
-import com.example.transportation.model.Vehicle;
-import com.example.transportation.service.VehicleService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 @RestController
-@RequestMapping("/vehicles")
+@RequestMapping("/vehicles") // Base path
 public class VehicleController {
-    private final VehicleService vehicleService;
 
-    public VehicleController(VehicleService vehicleService) {
-        this.vehicleService = vehicleService;
+    @Autowired
+    private VehicleService vehicleService;
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Vehicle Management System";
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public List<Vehicle> getAllVehicles() {
         return vehicleService.getAllVehicles();
-    }
-
-    @PostMapping
-    public Vehicle addVehicle(@RequestBody Vehicle vehicle) {
-        return vehicleService.saveVehicle(vehicle);
     }
 }
